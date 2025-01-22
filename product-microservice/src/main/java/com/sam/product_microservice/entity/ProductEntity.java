9f094b5162891a23b1d92e5f1c467e2b52ffaeb3
DEVSAM1966 <desarrollo.devsam1966@gmail.com>
2025-01-23 00:37:03 +0100

    22/01/2025

--- product-microservice/src/main/java/com/sam/product_microservice/entity/ProductEntity.java
+++ product-microservice/src/main/java/com/sam/product_microservice/entity/ProductEntity.java
@@ -16,4 +16,12 @@ public class ProductEntity {
   private String productName;
   private String productDescription;
   private Double unitPrice;
+
+ public void setId(String id) { this.id = id; }
+  public String getProductName() { return productName; }
+  public void setProductName(String productName) { this.productName = productName; }
+  public String getProductDescription() { return productDescription; }
+  public void setProductDescription(String productDescription) { this.productDescription = productDescription; }
+  public Double getUnitPrice() { return unitPrice; }
+  public void setUnitPrice(Double unitPrice) { this.unitPrice = unitPrice; }
 }
